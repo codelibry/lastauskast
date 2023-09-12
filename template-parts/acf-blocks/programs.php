3bc37002949e2145447a1a9af2ddf838a8890bba
modifikult <dev@flooow.agency>
2023-09-12 12:30:22 +0300

    added inline background color for card programs

--- template-parts/acf-blocks/programs.php
+++ template-parts/acf-blocks/programs.php
@@ -5,21 +5,22 @@
 
 <?php if ($programs_posts) : ?>
     <section class="programs">
-        <?php if($programs_title) :?>
+        <?php if ($programs_title) : ?>
             <div class="programs__title">
-                <?php echo $programs_title;?>
+                <?php echo $programs_title; ?>
             </div>
         <?php endif; ?>
         <div class="container--medium">
             <div class="programs__wrap">
                 <div class="programs__list js-multiply-slider">
-                    <?php foreach($programs_posts as $post) :?>
-                        <?php setup_postdata($post);?>
-                        <div class="slider-item">
-                            <?php get_template_part('template-parts/feed/card', 'programs');?>
+                    <?php foreach ($programs_posts as $idx => $post) : ?>
+                        <?php setup_postdata($post); ?>
+                        <div class="slider-item"
+                             style="background-color: <?php echo $idx % 2 ? '#8d9c9a' : 'transparent'; ?>">
+                            <?php get_template_part('template-parts/feed/card', 'programs'); ?>
                         </div>
-                        <?php wp_reset_postdata();?>
-                    <?php endforeach;?>
+                        <?php wp_reset_postdata(); ?>
+                    <?php endforeach; ?>
                 </div>
             </div>
         </div>
