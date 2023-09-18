ab698738d995c0d3e7764b93aefa7b779ea02c5d
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2023-09-18 15:13:38 +0300

    Mokimay single page font sizes update

--- template-parts/single/content.php
+++ template-parts/single/content.php
@@ -8,7 +8,7 @@
     <section class="content animate fade-up">
         <div class="container--small">
             <div class="content__wrap">
-                <div class="content__text">
+                <div class="content__text h4">
                     <?php echo $content; ?>
                 </div>
                 <?php if ($contact_btn || $other_programs_btn) : ?>
