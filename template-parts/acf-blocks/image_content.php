9fd0ed0957d16bd41e92674c9e4303fea56fe9e7
modifikult <dev@flooow.agency>
2023-09-19 12:56:12 +0300

    added contetn block class for content block in image conten section

--- template-parts/acf-blocks/image_content.php
+++ template-parts/acf-blocks/image_content.php
@@ -13,7 +13,7 @@
                         <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?: $image['title']; ?>">
                     </div>
                 </div>
-                <div class="image-content__text <?php echo $image_position === 'right' ? 'text--left' : 'text--right'; ?>">
+                <div class="image-content__text <?php echo $image_position === 'right' ? 'text--left' : 'text--right'; ?> content-block">
                     <?php echo $content; ?>
                 </div>
             </div>
