84c7146fd033c5b6430a7f9018f3cfa98a525283
vitaliiflow <132272871+vitaliiflow@users.noreply.github.com>
2024-12-16 12:19:33 +0100

    Update single-programs.php
    Add h1 to programs page

--- single-programs.php
+++ single-programs.php
@@ -1,5 +1,18 @@
 <?php get_header(); ?>
 
+<section class="programs">
+	<div class="programs__title h4 animate fade-in">
+		<h1 class="h2" style="color: #d4d0bf;">
+			<?php echo the_title(); ?>
+		</h1>
+	</div>
+</section>
+<style>
+  #main {
+  	padding-top: 25px;
+  }
+</style>
+
 <?php get_template_part('template-parts/single/bg_text_image');?>
 
 <?php get_template_part('template-parts/single/content');?>
